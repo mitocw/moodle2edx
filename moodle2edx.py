5a9cdfffe62974cd233ab33982b2705ffa7e7d5d
ichuang <ichuang@mit.edu>
2014-02-09 00:15:53 -0500

    handle resources, url, label activities

--- moodle2edx.py
+++ moodle2edx.py
@@ -10,6 +10,7 @@
 
 import os, sys, string, re
 import codecs
+import tempfile
 from lxml import etree
 from abox import AnswerBox
 from path import path
@@ -30,10 +31,10 @@ class Moodle2Edx(object):
         if infn.endswith('.mbz'):
             # import gzip, tarfile
             # dir = tarfile.TarFile(fileobj=gzip.open(infn))
-            dir = tempfile.mkdtemp(prefix="moodle2edx")
+            mdir = tempfile.mkdtemp(prefix="moodle2edx")
             curdir = os.path.abspath('.')
-            os.chdir(dir)
-            os.system('tar xzf %s' % infn)
+            os.chdir(mdir)
+            os.system('tar xzf %s/%s' % (curdir, infn))
             os.chdir(curdir)
         else:
             mdir = infn
@@ -45,6 +46,8 @@ class Moodle2Edx(object):
         self.verbose = verbose
         self.edxdir = path(edxdir)
         self.moodle_dir = path(mdir)
+        if not self.edxdir.exists():
+            os.mkdir(self.edxdir)
         def mkdir(mdir):
             if not os.path.exists('%s/%s' % (self.edxdir, mdir)):
                 os.mkdir(self.edxdir / mdir)
@@ -58,6 +61,7 @@ class Moodle2Edx(object):
         qfn = 'questions.xml'	# moodle questions xml
     
         qdict = self.load_questions(mdir,qfn)
+        self.convert_static_files()
     
         moodx = etree.parse('%s/%s' % (mdir,mfn))
     
@@ -65,6 +69,7 @@ class Moodle2Edx(object):
         name = info.find('.//original_course_fullname').text
         number = info.find('.//original_course_shortname').text
         contents = moodx.find('.//contents')
+        number = self.make_url_name(number, extra_ok_chars='.')
     
         # start course.xml
         cxml = etree.Element('course', graceperiod="1 day 5 hours 59 minutes 59 seconds")
@@ -77,8 +82,11 @@ class Moodle2Edx(object):
         # sections is dict with key=sectionid, value=chapter XML
         sections = {}
 
+        self.load_moodle_course_head(cxml)	# load the course/course.xml if it has anything
+
+        seq = None
         for activity in contents.findall('.//activity'):
-            self.activity2chapter(activity, sections, cxml, qdict)
+            seq = self.activity2chapter(activity, sections, cxml, seq, qdict)
             
         chapter = cxml.find('chapter')
         name = name.replace('/',' ')
@@ -91,38 +99,59 @@ class Moodle2Edx(object):
         # the actual top-level course.xml file is a pointer XML file to the one in course/semester.xml
         open('%s/course.xml' % cdir, 'w').write('<course url_name="%s" org="%s" course="%s"/>\n' % (semester, org, number))
 
-        self.convert_static_files()
-
-    # convert static files
 
     def convert_static_files(self):
         '''
         Convert moodle static files (from moodle_dir/files/prefix/hash) to edX (edxdir/static/file_name)
         use moodle_dir/files.xml
+
+        Construct dictionary of files by id, with values as static link urls.
+
+        Save that dictionary as self.staticfiles
         '''
+        self.staticfiles = {}
         fxml = etree.parse(self.moodle_dir / 'files.xml').getroot()
         if self.verbose:
             print "==== Copying static files"
         for mfile in fxml.findall('file'):
             fhash = mfile.find('contenthash').text
             ftype = mfile.find('mimetype').text
-            fname = mfile.find('filename').text
+            fname = mfile.find('filename').text	    # instructor supplied filename
             if fname=='.':
                 # print "    strange filename '.', skipping..."
                 continue
             fname2 = fname.replace(' ', '_')
+            fileid = mfile.get('id')
+            url = '/static/%s' % fname2
+            self.staticfiles[fileid] = (url, fname)
             os.system('cp %s/files/%s/%s "%s/static/%s"' % (self.moodle_dir, fhash[:2], fhash, self.edxdir, fname2))
             if self.verbose:
                 print "      %s" % fname
                 sys.stdout.flush()
             
 
+    def load_moodle_course_head(self, cxml):
+        '''
+        load the course/course.xml if it has anything
+        '''
+        xml = etree.parse('%s/course/course.xml' % (self.moodle_dir)).getroot()
+        name = xml.find('shortname').text
+        contents = xml.find('summary').text
+        if not contents:
+            return
+        
+        chapter = etree.SubElement(cxml,'chapter')
+        seq = etree.SubElement(chapter,'sequential')
+        seq.set('display_name', name)
+        url_name = self.make_url_name('course__' + name, dupok=False)
+        self.save_as_html(url_name, name, contents, seq)
+
     # convert activity to chapter
     
-    def activity2chapter(self, activity, sections, cxml, qdict):
+    def activity2chapter(self, activity, sections, cxml, seq, qdict):
 
         adir = activity.find('directory').text
-        title = activity.find('title').text
+        title = activity.find('title').text.strip()
         category = activity.find('modulename').text
         sectionid = activity.find('sectionid').text
         
@@ -130,26 +159,54 @@ class Moodle2Edx(object):
         if not sectionid in sections:
             chapter = etree.SubElement(cxml,'chapter')
             sections[sectionid] = chapter
-            self.get_moodle_section(sectionid, chapter)
+            seq = self.get_moodle_section(sectionid, chapter, activity_title=title)
         else:
             chapter = sections[sectionid]
     
         if category=='url':
-            print "--> URL %s (%s)" % (title,adir)
-            print "*** skipping"
+            if seq is None:		# use current sequential if exists, else create new one
+                seq = etree.SubElement(chapter,'sequential')
+            else:
+                print "  ",
+            print "  --> URL %s (%s)" % (title,adir)
+            self.import_moodle_url(adir, seq)
+
+        elif category=='label':
+            if seq is None:		# use current sequential if exists, else create new one
+                seq = etree.SubElement(chapter,'sequential')
+            else:
+                print "  ",
+            print "  --> label %s (%s)" % (title,adir)
+            self.import_moodle_label(adir, seq)
+
+        elif category=='resource':
+            if seq is None:		# use current sequential if exists, else create new one
+                seq = etree.SubElement(chapter,'sequential')
+            else:
+                print "  ",
+            print "  --> resource %s (%s)" % (title,adir)
+            self.import_moodle_resource(adir, seq)
+
         elif category=='page':
-            print "--> etext %s (%s)" % (title,adir)
+            print "  --> etext %s (%s)" % (title,adir)
             seq = etree.SubElement(chapter,'sequential')
             self.import_page(adir, seq)
     
         elif category=='quiz':
-            print "--> problem %s (%s)" % (title,adir)
-            chapter = etree.SubElement(cxml,'chapter')
-            chapter.set('display_name',title)
-            seq = etree.SubElement(chapter,'sequential')
+            if seq is None:		# use current sequential if exists, else create new one
+                seq = etree.SubElement(chapter,'sequential')
+            else:
+                print "  ",
+            print "  --> problem %s (%s)" % (title,adir)
             self.import_quiz(adir,seq,qdict)
 
-    def get_moodle_section(self, sectionid, chapter):
+        else:
+            print "  --> unknown activity type %s (adir=%s)" % (category, adir)
+
+        return seq
+
+
+    def get_moodle_section(self, sectionid, chapter, activity_title=""):
         '''
         sectionid is a number
         '''
@@ -165,15 +222,21 @@ class Moodle2Edx(object):
                 name = html2text.html2text(m.group(2))
                 name = name.replace('\n','').replace('\r','')
         if not name or name=='$@NULL@$':
-                print "Warning: empty name for section %s, contents=%s ..." %  (sectionid, contents[:100])
-                name = html2text.html2text(contents)[:50].split('\n')[0].strip()
+            htext = html2text.html2text(contents)
+            # print "Warning: empty name for section %s, contents=%s ..." %  (sectionid, htext.split('\n')[0].strip())
+            name = htext[:50].split('\n')[0].strip()
+        if not name:
+            name = activity_title.strip().split('\n')[0].strip()
         name = name.strip()
+        print "--> Section: %s" % name
         chapter.set('display_name', name)
         if contents:
             seq = etree.SubElement(chapter,'sequential')
             seq.set('display_name', name)
-            url_name = self.make_url_name(name, dupok=False)
+            url_name = self.make_url_name('section_%s__%s' % (sectionid, name), dupok=False)
             self.save_as_html(url_name, name, contents, seq)
+            return seq
+        return None
                 
     def get_moodle_page_by_id(self, moduleid):
         '''
@@ -182,12 +245,39 @@ class Moodle2Edx(object):
         adir = 'activities/page_%s' % moduleid
         return self.get_moodle_page_by_dir(adir)
 
-    def get_moodle_page_by_dir(self, adir):
-        pxml = etree.parse('%s/%s/page.xml' % (self.moodle_dir, adir)).getroot()
-        name = pxml.find('.//name').text
+    def get_moodle_page_by_dir(self, adir, fn='page.xml'):
+        pxml = etree.parse('%s/%s/%s' % (self.moodle_dir, adir, fn)).getroot()
+        name = pxml.find('.//name').text.strip().split('\n')[0].strip()
         fnpre = os.path.basename(adir) + '__' + name.replace(' ','_').replace('/','_')
         url_name = self.make_url_name(fnpre, dupok=True)
         return pxml, url_name, name
+
+    def import_moodle_url(self, adir, seq):
+        pxml, url_name, name = self.get_moodle_page_by_dir(adir, fn='url.xml')
+        seq.set('display_name', name)
+        url = cgi.escape(pxml.find('.//externalurl').text)
+        # html.set('display_name', name)
+        htmlstr = pxml.find('.//intro').text or ''
+        htmlstr = '<p>%s</p><p><a href="%s">%s</a></p>' % (htmlstr, url, name)
+        self.save_as_html(url_name, name, htmlstr, seq)
+
+    def import_moodle_label(self, adir, seq):
+        pxml, url_name, name = self.get_moodle_page_by_dir(adir, fn='label.xml')
+        seq.set('display_name', name)
+        htmlstr = pxml.find('.//intro').text
+        self.save_as_html(url_name, name, htmlstr, seq)
+
+    def import_moodle_resource(self, adir, seq):
+        pxml, url_name, name = self.get_moodle_page_by_dir(adir, fn='resource.xml')
+        seq.set('display_name', name)
+        xml = etree.parse('%s/%s/%s' % (self.moodle_dir, adir, 'inforef.xml')).getroot()
+        htmlstr = '<h2>%s</h2>' % cgi.escape(name)
+        for fileid in xml.findall('.//id'):
+            fidnum = fileid.text
+            (url, filename) = self.staticfiles.get(fidnum, ('',''))
+            # print "fileid: %s -> %s" % (fidnum, self.staticfiles.get(fidnum))
+            htmlstr += '<p><a href="%s">%s</a></p>' % (url, filename)
+        self.save_as_html(url_name, name, htmlstr, seq)
 
     def import_page(self, adir, seq):
         pxml, url_name, name = self.get_moodle_page_by_dir(adir)
@@ -225,6 +315,7 @@ class Moodle2Edx(object):
         htmlstr = (u'<html display_name="%s">\n' % cgi.escape(name)) + htmlstr + u'\n</html>'
         codecs.open('%s/html/%s.xml' % (self.edxdir, url_name),'w',encoding='utf8').write(htmlstr)
         html.set('url_name','%s' % url_name)
+        vert.set('url_name', 'vert_%s' % url_name)
     
     def import_quiz(self, adir,seq,qdict):
         qxml = etree.parse('%s/%s/quiz.xml' % (self.moodle_dir, adir)).getroot()
@@ -317,7 +408,7 @@ class Moodle2Edx(object):
     
     #----------------------------------------
     
-    def make_url_name(self, s, tag='', dupok=False):
+    def make_url_name(self, s, tag='', dupok=False, extra_ok_chars=""):
         '''
         Turn string s into a valid url_name.
         Use tag if provided.
@@ -338,9 +429,12 @@ class Moodle2Edx(object):
             for ch in m:
                 s = s.replace(ch,v)
 
+        if s=='':
+            s = 'x'
+
         snew = ''
         for ch in s:
-            if not ch in string.lowercase + string.uppercase + string.digits + '-_ ':
+            if not ch in string.lowercase + string.uppercase + string.digits + '-_ ' + extra_ok_chars:
                 ch = ''
             snew += ch
         s = snew
